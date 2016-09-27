1562cf3b998e3b44478ce92191f50b3a5d24e274
Qwertyasd2016 <xenjin1323@gmail.com>
2016-09-27 10:31:01 +0300

    -

--- task_12.py
+++ task_12.py
@@ -5,7 +5,12 @@ from pyrob.api import *
 
 @task
 def task_8_6():
-    pass
+    while wall_is_on_the_right()==False:
+        if wall_is_above()==False and wall_is_beneath()==True:
+            fill_cell()
+        move_right(n=1)
+    if wall_is_above()==False and wall_is_beneath()==True:
+        fill_cell()
 
 
 if __name__ == '__main__':
--- task_14.py
+++ task_14.py
@@ -5,7 +5,20 @@ from pyrob.api import *
 
 @task
 def task_8_11():
-    pass
+    while wall_is_on_the_right()==False:
+        if wall_is_beneath()==False:move_down(n=1); fill_cell(); move_up(n=1)
+        if wall_is_above()==False:move_up(n=1); fill_cell(); move_down(n=1)
+        if wall_is_above()==True and wall_is_beneath()==True:fill_cell()
+        move_right(n=1)
+    if wall_is_beneath()==False:
+            move_down(n=1)
+            fill_cell()
+            move_up(n=1)
+    if wall_is_above()==False:
+            move_up(n=1)
+            fill_cell()
+            move_down(n=1)
+
 
 
 if __name__ == '__main__':
