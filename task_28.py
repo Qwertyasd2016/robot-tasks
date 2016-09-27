d7c69f3b0a13133dd8bfb145a2a861312267c408
Qwertyasd2016 <xenjin1323@gmail.com>
2016-09-27 12:15:34 +0300

    -

--- task_28.py
+++ task_28.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    i=0
+    while i != 5:
+        if cell_is_filled()==True:
+            i+=1
+        if i==5: break
+        if wall_is_on_the_right()==True: break
+        else: move_right(n=1)
+
 
 
 if __name__ == '__main__':
