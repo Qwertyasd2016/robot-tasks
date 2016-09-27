65eaed74c97df4b802fe9f1f3117a0bf8b8b8c9b
Qwertyasd2016 <xenjin1323@gmail.com>
2016-09-27 12:08:05 +0300

    -

--- task_24.py
+++ task_24.py
@@ -5,7 +5,8 @@ from pyrob.api import *
 
 @task
 def task_2_1():
-    pass
+
+
 
 
 if __name__ == '__main__':
--- task_27.py
+++ task_27.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task
 def task_7_5():
-    pass
+    move_right(n=1)
+    fill_cell()
+    a=0
+    while wall_is_on_the_right()==False:
+        a+=1
+        for i in range(a):
+            if wall_is_on_the_right()==False: move_right(n=1)
+        fill_cell()
+
+
 
 
 if __name__ == '__main__':
